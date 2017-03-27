bd1e9548d427ddf39745e2ab98c7c5ebd1eb4439
Ben Costrell <bencostrell@gmail.com>
2017-03-27 17:48:31 -0400

    block fade out task

---
+++ Smashout v2/Assets/Scripts/Level/BlockFadeOut.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockFadeOut : Task {
+
+    private GameObject block;
+    private float timeElapsed;
+    private float duration;
+    private Vector3 initialScale;
+
+	public BlockFadeOut(GameObject blk, float dur)
+    {
+        block = blk;
+        duration = dur;
+    }
+
+    protected override void Init()
+    {
+        timeElapsed = 0;
+        initialScale = block.transform.localScale;
+        block.GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    internal override void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        block.transform.localScale = Vector3.Lerp(initialScale, Vector3.zero, Easing.ExpoEaseIn(timeElapsed / duration));
+
+        if (timeElapsed >= duration)
+        {
+            SetStatus(TaskStatus.Success);
+        }
+    }
+
+    protected override void OnSuccess()
+    {
+        //Services.BlockManager.DestroyBlock(block);
+    }
+}
