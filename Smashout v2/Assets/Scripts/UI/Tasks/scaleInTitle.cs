eed75a1def7cc63f8dc134f8751f1373a64640b0
Ben Costrell <bencostrell@gmail.com>
2017-03-28 19:08:41 -0400

    ui testing scene

---
+++ Smashout v2/Assets/Scripts/UI/Tasks/scaleInTitle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleInTitle : Task {
+    private RectTransform title;
+    private float timeElapsed;
+    private float duration;
+
+    protected override void Init()
+    {
+        timeElapsed = 0;
+        duration = Services.UIManager.titleScaleInTime;
+        title = Services.UIManager.title.GetComponent<RectTransform>();
+    }
+
+    internal override void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        title.anchoredPosition = Vector2.Lerp(Vector2.zero, Vector2.one, Easing.ExpoEaseOut(timeElapsed / duration));
+
+        if (timeElapsed >= duration)
+        {
+            SetStatus(TaskStatus.Success);
+        }
+    }
+
+}
--- Smashout v2/Assets/Scripts/UI/UIManager.cs
+++ Smashout v2/Assets/Scripts/UI/UIManager.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
+    public GameObject title;
+    public GameObject startPrompt;
+    public GameObject congrats;
+    public GameObject restartPrompt;
+    
+    // Use this for initialization
 	void Start () {
 		
 	}
